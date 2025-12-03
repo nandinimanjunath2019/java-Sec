120b3c8ed226edf8a7a951b1a92afff6fe4f05fe
nandinimanjunath2019 <nandinimanjunath2019@gmail.com>
2025-12-03 13:01:34 +0530

    Create LoopDemo.java

---
+++ LoopDemo.java
@@ -0,0 +1,14 @@
+public class LoopDemo {
+    public static void main(String[] args) {
+        int i = 1;
+        int j =1;
+        while(j<=10);
+        System.out.println(j);
+        j++;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
