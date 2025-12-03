0b7267ac5d42cbea9962285db36ad5e7ba3a8e5a
nandinimanjunath2019 <nandinimanjunath2019@gmail.com>
2025-12-03 13:04:07 +0530

    Create Dowhileloop.java

---
+++ Dowhileloop.java
@@ -0,0 +1,10 @@
+public class Dowhileloop {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println("Do-while count:"+i);
+            i++;
+        }while(i<=5);
+    }
+        }
+    
