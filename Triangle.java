95fb364776555a2e187c2842f13d1d89cca35480
nandinimanjunath2019 <nandinimanjunath2019@gmail.com>
2025-12-03 13:15:48 +0530

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,13 @@
+import java. util.Scanner;
+ public class Triangletype {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int c = 5;
+        if(a ==b & b==c){
+            System.out.println("Equilateral Triangle");
+        }else{
+            System.out.println("Not an Equilateral Triangle");
+        }
+    }
+}   
