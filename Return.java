060856fd8a2e1f0e555751e4565dd5893de59c87
nandinimanjunath2019 <nandinimanjunath2019@gmail.com>
2025-12-03 13:09:23 +0530

    Create Return.java

---
+++ Return.java
@@ -0,0 +1,9 @@
+ublic class Return {
+    public static void main(String[] args) {
+        System.out.println("Before return");
+        if(true)
+        return;
+        System.out.println("Affter return");
+
+    }
+}
